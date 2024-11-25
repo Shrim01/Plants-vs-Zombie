b0cf7e13841782554fcfc27fe2698487f83cad43
Shrim01 <shrim01@mail.ru>
2024-11-26 02:46:10 +0500

    moving

--- Assets/Scripts/GoroxScript.cs
+++ Assets/Scripts/GoroxScript.cs
@@ -12,18 +12,21 @@ public class GoroxScript : MonoBehaviour
 
     void Start()
     {
-
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        if (horizontal!=0)
-            rb2D.AddForce(new Vector2(horizontal * speed,0));
-        if (vertical!=0)
+        if (horizontal != 0)
+            rb2D.AddForce(new Vector2(horizontal * speed, 0));
+        if (vertical != 0)
             rb2D.AddForce(new Vector2(0, vertical * speed));
-        Debug.Log(rb2D.velocity);
-        rb2D.velocity*= new Vector2(resist,resist);
+        rb2D.velocity *= new Vector2(resist, resist);
+        var position = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        var rotation = Mathf.Atan(position.y / position.x)* 180 / Mathf.PI +90;
+        if (position.x>0)
+            rotation-=180;
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
     }
 }
