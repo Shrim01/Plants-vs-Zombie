1972b23a8ad5299c86fe36d2456f1353d7ecfa3f
Shrim01 <shrim01@mail.ru>
2024-11-28 15:27:47 +0500

    bullet

--- Assets/GoroxScript.cs
+++ Assets/GoroxScript.cs
@@ -16,6 +16,11 @@ public class GoroxScript : MonoBehaviour
 
     void Update()
     {
+        Move();
+        Rotate();
+    }
+    private void Move()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         if (horizontal != 0)
@@ -23,6 +28,9 @@ public class GoroxScript : MonoBehaviour
         if (vertical != 0)
             rb2D.AddForce(new Vector2(0, vertical * speed));
         rb2D.velocity *= new Vector2(resist, resist);
+    }
+    private void Rotate()
+    {
         var position = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
         var rotation = Mathf.Atan(position.y / position.x)* 180 / Mathf.PI +90;
         if (position.x>=0)
---
+++ Assets/ShootScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public GameObject bulllet;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
