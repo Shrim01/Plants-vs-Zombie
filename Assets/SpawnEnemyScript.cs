4b4d44d3f904f1b5601ad4851940145f657b566d
Shrim01 <shrim01@mail.ru>
2024-11-29 03:44:02 +0500

    Stupid Zombie

--- Assets/SpawnEnemyScript.cs
+++ Assets/SpawnEnemyScript.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEditor.Build.Content;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnEnemyScript : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject Zombie;
-    public GameObject Plants;
-    private int[] countEnemy = { 0, 0 };
+    public GameObject player;
+    public GameObject zombie;
+    public GameObject plants;
+    public int CountZombieOnMap = 15;
+    public int CountPlantsOnMap;
+    private Dictionary<string, int> countEnemy = new() { { "Zombie", 0 }, { "Plants", 0 } };
+    private int x = 12;
+    private int y = 6;
 
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        SpawnZombie();
+    }
 
+    private void SpawnZombie()
+    {
+        if (countEnemy["Zombie"]++ < CountZombieOnMap)
+            Instantiate(zombie, GetPosition(), transform.rotation);
+        countEnemy["Zombie"] = GameObject.FindGameObjectsWithTag("Zombie").Length;
+    }
+
+    private Vector3 GetPosition()
+    {
+        var radius = Random.Range(Mathf.Sqrt(x * x + y * y) + 2, x * 2.5f);
+        var rotation = Random.rotation.eulerAngles.z;
+        return player.transform.position + new Vector3(
+            Mathf.Cos(rotation * Mathf.PI / 180) * radius,
+            Mathf.Sin(rotation * Mathf.PI / 180) * radius, 0);
     }
 }
--- Assets/ZombieScript.cs
+++ Assets/ZombieScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ZombieScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private int x = 12;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var position =player.transform.position-transform.position;
+        if (Mathf.Sqrt(position.x*position.x+position.y*position.y)>x*3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
