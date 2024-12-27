4319f85dcc86666d555f392ceba09ff3937aaf88
Shrim01 <shrim01@mail.ru>
2024-12-28 00:35:00 +0500

    comit

--- Assets/GamePlay/Star/StarScript.cs
+++ Assets/GamePlay/Star/StarScript.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class StarScript : MonoBehaviour
+public class PeasScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float reload;
+    public GameObject bullet;
+    public int damageBullet;
+    public int speedBullet;
+    private float MaxHealthPoint = 100;
+    private float CurrentHealthPoint;
+    private float timer;
+
     void Start()
     {
-        
+        CurrentHealthPoint = MaxHealthPoint;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetMouseButton(0) && timer > reload)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Zombie")
+            CurrentHealthPoint -= other.gameObject.GetComponent<ZombieScripts>().damage * Time.deltaTime;
+        if (CurrentHealthPoint <= 0)
+        {
+            gameObject.SetActive(false);
+            GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>().GameOver();
+        }
     }
 }
